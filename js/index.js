847d91e04d97d5c22624f6d26eb19c3e83b718bd
rlarlqja1 <89184644+rlarlqja1@users.noreply.github.com>
2024-04-27 09:32:22 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(`.menu > li`).mouseover(function() {
+    $(this).children(`.submenu`).stop().slideDown();
+});
+
+$(`.menu > li`).mouseleave(function() {
+    $(this).children(`.submenu`).stop().slideUp();
+});

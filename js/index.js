fac26ea1465ac8090b948897fbe51fcbf43bd5bb
rlarlqja1 <89184644+rlarlqja1@users.noreply.github.com>
2024-04-28 10:06:36 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-$(`.menu > li`).mouseover(function() {
-    $(this).children(`.submenu`).stop().slideDown();
-});
+$(".c_i,.back1").mouseover(function(){
+    $(".back1").show();
+    $("#cg1").show();
+})
 
-$(`.menu > li`).mouseleave(function() {
-    $(this).children(`.submenu`).stop().slideUp();
-});
+$(".c_b,.back2").mouseover(function(){
+    $(".back2").show();
+    $("#cg2").show();
+})
+
+$(".c_m,.back3").mouseover(function(){
+    $(".back3").show();
+    $("#cg3").show();
+})
+
+
+$(".c_i,.back1").mouseleave(function(){
+    $(".back1").hide();
+    $("#cg1").hide();
+})
+
+$(".c_b,.back2").mouseleave(function(){
+    $(".back2").hide();
+    $("#cg2").hide();
+})
+
+$(".c_m,.back3").mouseleave(function(){
+    $(".back3").hide();
+    $("#cg3").hide();
+})
+
